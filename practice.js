d8e3cb68d79bf252b1ee2587a1abecebd890dba5
Elly <ellychau10@yahoo.com>
2021-05-25 16:02:05 -0700

    Final Commit

--- practice.js
+++ practice.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", function(){
+    const cv = document.getElementById('mycanvas');
 
+    cv.width = 500;
+    cv.height = 500;
+
+    const ctx = cv.getContext('2d');
+
+    // Rectangle
+    ctx.fillStyle = 'green';
+    ctx.fillRect(10,10,80,50);
+
+    // Circle
+    ctx.beginPath();
+    ctx.arc(100,100,50,0,5 * Math.PI);
+    ctx.strokeStyle = 'red';
+    ctx.lineWidth = 5;
+    ctx.stroke();
+
+    ctx.fillStyle = 'orange';
+    ctx.fill();
 });
